a6f99d047ea90487c0b082e1c4c950db2eaf64d4
ArnavSaikia <arnavsaikia817@gmail.com>
2025-06-15 23:26:18 +0530

    All categories added. Time to work on footer soon

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './Components/Navbar.jsx';
 import Funding from './Components/Funding.jsx';
 import Navbar2 from './Components/Navbar2.jsx';
 import Headlines from './Components/Headlines.jsx';
+import Category from './Components/Category.jsx';
 
 function App() {
   return (
@@ -14,6 +15,14 @@ function App() {
       <Funding/>
       <Navbar2/>
       <Headlines/>
+      <Category name="United States"/>
+      <Category name="World"/>
+      <Category name="Sport"/>
+      <Category name="Business"/>
+      <Category name="Entertainment"/>
+      <Category name="Science"/>
+      <Category name="Travel"/>
+      <Category name="Food"/>
     </>
   )
 }
--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -1,12 +1,14 @@
 import Article from "./Article.jsx";
+import '../Styles/Category.css'
 
 function Category(props){
     return (
         <div className="category-box">
-            <span>{props.name} &gt; </span>
+            <span>{props.name}&nbsp;&nbsp;&gt; </span>
             <Article/>
             <Article/>
             <Article/>
+            <hr/>
         </div>
     )
 }
