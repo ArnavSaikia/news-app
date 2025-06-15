3cfb7adca04c639ea5e9eb2cd0705cf9fa4c6e6a
ArnavSaikia <arnavsaikia817@gmail.com>
2025-06-15 12:15:38 +0530

    navbar slider created and previous font weights tweaked

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Components/Navbar.jsx';
 import Funding from './Components/Funding.jsx';
+import Navbar2 from './Components/Navbar2.jsx';
 
 function App() {
   return (
     <>
       <Navbar/>
       <Funding/>
+      <Navbar2/>
     </>
   )
 }
---
+++ src/Components/Navbar2.jsx
@@ -0,0 +1,26 @@
+import '../Styles/Navbar2.css';
+
+function Navbar2(){
+    return(
+        <div className="navbar2">
+            <div>Home</div>
+            <div>News</div>
+            <div>Opinion</div>
+            <div>Sport</div>
+            <div>Culture</div>
+            <div>Lifestyle</div>
+            <div className="vertical-line"></div>
+            <div>World</div>
+            <div>Climate Crisis</div>
+            <div>Environment</div>
+            <div>Science</div>
+            <div>Global Development</div>
+            <div>Tech</div>
+            <div>Business</div>
+            <div className="vertical-line"></div>
+            <div>More</div>
+        </div>
+    )
+}
+
+export default Navbar2;
