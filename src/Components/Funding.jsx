0745901960756de9478f6e2638a03cb2cf8fbbd5
ArnavSaikia <arnavsaikia817@gmail.com>
2025-06-18 01:37:41 +0530

    added media queries for funding section for tablet devices

--- src/Components/Funding.jsx
+++ src/Components/Funding.jsx
@@ -1,10 +1,15 @@
-import '../Styles/Funding.css'
+import '../Styles/Funding.css';
+import guardianLogo from '../assets/guardianlogo.png';
 
 function Funding(){
     return(
         <div className="funding-box">
-            <span>Support The Guardian</span>
-            <button>Support Us</button>
+            <div>
+                <span>Support The Guardian</span>
+                <span>Fund independent journalism with $5 per month</span>
+                <button>Support Us</button>
+            </div>
+            <img src={guardianLogo}></img>
         </div>
     )
 }
