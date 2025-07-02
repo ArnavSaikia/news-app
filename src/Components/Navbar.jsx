495038d2403f204cd52c842382edea4f8978b8ee
ArnavSaikia <arnavsaikia817@gmail.com>
2025-07-02 23:01:44 +0530

    added a hyperlink to the logo img and changed article page styling

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,14 @@
 import guardianLogo from '../assets/guardianlogo_white.png';
 import SearchWidget from './SearchWidget.jsx';
 import WeatherWidget from './WeatherWidget.jsx';
+import { Link } from 'react-router-dom';
 import '../App.css';
 
 function Navbar(){
     return(
         <nav>
             <WeatherWidget/>
-            <img src={guardianLogo} className='logo'></img>
+            <Link to='/' style={{display: 'contents'}}><img src={guardianLogo} className='logo'></img></Link>
             <SearchWidget/>
         </nav>
     )
