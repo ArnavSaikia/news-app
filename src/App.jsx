e0947c645a6bec778ef1127266f0dd27a5180b61
ArnavSaikia <arnavsaikia817@gmail.com>
2025-07-04 13:02:18 +0530

    small script to scroll to top on every route change

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Landing from './Pages/Landing.jsx';
 import CategoryPage from './Pages/CategoryPage.jsx';
 import ArticlePage from './Pages/ArticlePage.jsx';
 import SearchPage from './Pages/SearchPage.jsx';
+import ScrollToTop from './Components/ScrollToTopScript.jsx';
 
 
 function App() {
 
   return (
     <Router basename='/news-app'>
+      <ScrollToTop/>
       <Routes>
         <Route path='/' element={<Landing/>}/>
         <Route path='/category/:id' element={<CategoryPage/>}/>
